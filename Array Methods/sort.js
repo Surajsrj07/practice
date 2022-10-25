931857fba83772bca75852ae258e22b07d2d5b7d
Surajsrj07 <Surajsrj93@gmail.com>
2022-10-25 21:03:00 +0530

    sort

---
+++ Array Methods/sort.js
@@ -0,0 +1,85 @@
+//Sort
+
+//ascending or descending 
+
+const names = ["suraj", "dhiraj", "manoj", "meghana", "akshay"];
+console.log("Before sort : " + names);
+names.sort();
+console.log("After sort : " + names);
+
+//it takes only first character
+
+let num = [25, 45, 24, 11, 37, 8, 19];
+console.log("Before sort : " + num);
+num.sort();
+console.log("After sort : " + num); //11,19,24,25,37,45,8
+
+//Ascending order in numbers
+num = [25, 45, 24, 11, 37, 8, 19];
+console.log("Before sort : " + num);
+num.sort((a, b) => {
+    return a - b;
+});
+console.log("After sort : " + num); //8,11,19,24,25,37,45
+
+//descending order
+num = [25, 45, 24, 11, 37, 8, 19];
+console.log("Before sort : " + num);
+num.sort((a, b) => {
+    return b - a;
+});
+console.log("After sort : " + num); //8,11,19,24,25,37,45
+
+
+
+const users = [
+    {
+        full_name: "suraj",
+        age: 38,
+        city: "salem",
+        salary: 10000,
+    },
+
+    {
+        full_name: "Manoj",
+        age: 15,
+        city: "Udupi",
+        salary: 15000,
+    },
+
+    {
+        full_name: "Dhiraj",
+        age: 44,
+        city: "salem",
+        salary: 16000,
+    },
+
+    {
+        full_name: "Akshay",
+        age: 12,
+        city: "Madurai",
+        salary: 12000,
+    },
+
+];
+
+
+//ascending Age format
+console.table(users);
+
+users.sort((a, b) => {
+    return a.age - b.age;
+})
+
+console.table(users);
+
+//ascending full name format 
+
+users.sort((a,b)=>{
+    if (a.full_name>b.full_name) return 1;
+    if (b.full_name>a.full_name) return -1;
+    return 0;
+});
+
+console.table(users);
+
