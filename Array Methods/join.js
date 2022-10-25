06ba7853fe26d3fbd0caa8f6b83350d5b26c097b
Surajsrj07 <Surajsrj93@gmail.com>
2022-10-25 22:01:20 +0530

    join

---
+++ Array Methods/join.js
@@ -0,0 +1,11 @@
+//array.join(seperator)
+
+//-------- default is seperate 
+
+const products =["Pencil","pen","Eraser","sharpener"];
+
+console.log(products);
+
+console.log(products.join());  //array to single string
+console.log(products.join('|'));
+console.log(products.join('+'));
